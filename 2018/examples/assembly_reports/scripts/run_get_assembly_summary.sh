284b10072214b75775cdb3d57c317e4acd65cc46
Haruo Suzuki <haruo@dotinstall.com>
2018-12-28 00:19:54 +0900

    remove 2018/examples

--- 2018/examples/assembly_reports/scripts/run_get_assembly_summary.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Downloading ASSEMBLY_REPORTS/
-wget -P data/ -o data/wget-log.txt -A "*.txt,*.txt.gz" -nd -r -np ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/
